aa97a53be53a7b79fa8ce2281440922c229c7e5f
Ivar Derksen <ivarderksen@hotmail.nl>
2020-10-05 11:48:26 +0200

    Added redux for fetching and managing host portal state

---
+++ host-portal/src/store/login_state.js
@@ -0,0 +1,23 @@
+const initialState = {
+  loading: false,
+  state: 'unknown',
+};
+
+export default function(state = initialState, action) {
+  switch(action.type) {
+    case 'errorLoggingIn':
+    case 'loggedOut':
+      return {
+        ...state,
+        state: 'loggedOut',
+        irmaSession: action.irmaSession,
+      };
+    case 'loggedIn':
+      return {
+        ...state,
+        state: 'loggedIn',
+      };
+    default:
+      return state;
+  }
+}
