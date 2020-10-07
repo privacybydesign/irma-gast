00d7ec7cb39ccd42c4d177e9926780eedd0b4b13
Hanna <git@creativecode.org>
2020-10-07 11:41:52 +0200

    Error page

--- front-end/src/components/error-page/Error.jsx
+++ front-end/src/components/error-page/Error.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import errorImg from "../../images/error.png";
 
 function Error(props) {
   return (
     <div className="content">
-      De QR-code is niet meer geldig, mogelijk omdat deze bijeenkomst afgelopen
-      is of de laatste twee weken geen bezoekers meer heeft gehad (en daarmee
-      automatisch opgeheven is). Klopt er iets niet? Neem dan contact op met de
-      host, via het e-mailadres boven de QR-code.
+      <h3>QR-code niet herkent</h3>
+      <br /> De QR-code is niet meer geldig, mogelijk omdat deze bijeenkomst
+      afgelopen is of de laatste twee weken geen bezoekers meer heeft gehad (en
+      daarmee automatisch opgeheven is). Klopt er iets niet? Neem dan contact op
+      met de host, via het e-mailadres boven de QR-code.
+      <br /> <br />
+      <div className="flex-item-single">
+        <img
+          className="illustration"
+          src={errorImg}
+          alt="Restaurant visitors"
+        />
+      </div>
     </div>
   );
 }
