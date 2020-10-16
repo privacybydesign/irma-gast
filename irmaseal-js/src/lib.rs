d48afa7661a3183587f19d5aeca49175e10b49bc
Leon <leon.botros@ru.nl>
2020-10-16 17:22:09 +0200

    rustfmt

--- irmaseal-js/src/lib.rs
+++ irmaseal-js/src/lib.rs
@@ -1,25 +1,25 @@
 use wasm_bindgen::prelude::*;
 
 use irmaseal_core::api::*;
-use irmaseal_core::{Identity,Writable,Readable,Error,UserSecretKey};
-use irmaseal_core::stream::{Sealer,OpenerSealed};
+use irmaseal_core::stream::{OpenerSealed, Sealer};
+use irmaseal_core::{Error, Identity, Readable, UserSecretKey, Writable};
 
-use js_sys::{Uint8Array,Date,Number};
+use js_sys::{Date, Number, Uint8Array};
 
-use std::io::{Cursor, SeekFrom, Seek, Write, Read};
 use std::cmp::min;
+use std::io::{Cursor, Read, Seek, SeekFrom, Write};
 
 // Wrap Cursor<Vec<u8>> to be a Writable
 struct Buf {
     pub c: Cursor<Vec<u8>>,
-    buf: [u8;1024]
+    buf: [u8; 1024],
 }
 
 impl Buf {
     pub fn new(v: Vec<u8>) -> Buf {
         let c = Cursor::<Vec<u8>>::new(v);
-        let buf = [0u8;1024];
-        Buf{c,buf}
+        let buf = [0u8; 1024];
+        Buf { c, buf }
     }
 }
 
@@ -32,7 +32,7 @@ impl Writable for Buf {
 
 impl Readable for Buf {
     fn read_byte(&mut self) -> Result<u8, Error> {
-        let mut x = [0u8;1];
+        let mut x = [0u8; 1];
         self.c.read_exact(&mut x[..]).unwrap();
         Ok(x[0])
     }
@@ -53,21 +53,12 @@ impl Readable for Buf {
 pub fn encrypt(whom: &str, what: &Uint8Array, pars: &str) -> Uint8Array {
     let now = (Date::now() as u64) / 1000;
     let mut rng = rand::thread_rng();
-    let id = Identity::new(
-        now,
-        "pbdf.pbdf.email.email",
-        Some(whom),
-    ).unwrap();
-    let ppars : Parameters = serde_json::from_str(pars).unwrap();
+    let id = Identity::new(now, "pbdf.pbdf.email.email", Some(whom)).unwrap();
+    let ppars: Parameters = serde_json::from_str(pars).unwrap();
     let mut buf = Buf::new(Vec::<u8>::new());
 
     {
-        let mut sealer = Sealer::new(
-            &id,
-            &ppars.public_key,
-            &mut rng,
-            &mut buf
-        ).unwrap();
+        let mut sealer = Sealer::new(&id, &ppars.public_key, &mut rng, &mut buf).unwrap();
         sealer.write(&what.to_vec()).unwrap();
     }
 
@@ -78,7 +69,7 @@ pub fn encrypt(whom: &str, what: &Uint8Array, pars: &str) -> Uint8Array {
 }
 
 // Extracts timestamp from the identity for the ciphertext.
-// 
+//
 // TODO Returning Number we loose some precission here, but not all browsers
 //      support BigInt64Array yet, which wasm_bindgen uses to return i64.
 #[wasm_bindgen]
@@ -94,8 +85,8 @@ pub fn extract_timestamp(ciphertext: &Uint8Array) -> Number {
 #[wasm_bindgen]
 pub fn decrypt(ciphertext: &Uint8Array, key: &str) -> Uint8Array {
     let (_, o) = OpenerSealed::new(Buf::new(ciphertext.to_vec())).unwrap();
-    let pkey : UserSecretKey = serde_json::from_str(
-        &serde_json::to_string(key).unwrap()[..]).unwrap();
+    let pkey: UserSecretKey =
+        serde_json::from_str(&serde_json::to_string(key).unwrap()[..]).unwrap();
     let mut o = o.unseal(&pkey).unwrap();
     let mut buf = Buf::new(Vec::<u8>::new());
     o.write_to(&mut buf);
