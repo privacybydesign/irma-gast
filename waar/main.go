746a517424d2972a8bb6fad633d240278eb8825c
Leon <leon.botros@ru.nl>
2020-10-21 12:50:26 +0200

    allow irma-welkom origin

--- waar/main.go
+++ waar/main.go
@@ -690,7 +690,7 @@ func main() {
 
 	// CORS setttings
 	cors := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:5000"},
+		AllowedOrigins:   []string{"http://localhost:5000", "https://irma-welkom.nl"},
 		AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodDelete},
 		AllowedHeaders:   []string{"Content-Type"},
 		AllowCredentials: true,
