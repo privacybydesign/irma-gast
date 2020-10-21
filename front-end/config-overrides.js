7bf806c4b28c9cd12d4c0028e2364c84bfdee7bd
Leon <leon.botros@ru.nl>
2020-10-21 15:55:54 +0200

    switch to wasm-pack-plugin for loading wasm

--- front-end/config-overrides.js
+++ front-end/config-overrides.js
@@ -1,25 +1,27 @@
+// Refer: https://github.com/lokesh-007/wasm-react-rust
+
 const path = require("path");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = function override(config, env) {
-  const wasmExtensionRegExp = /\.wasm$/;
-
   config.resolve.extensions.push(".wasm");
 
   config.module.rules.forEach((rule) => {
     (rule.oneOf || []).forEach((oneOf) => {
       if (oneOf.loader && oneOf.loader.indexOf("file-loader") >= 0) {
-        // make file-loader ignore WASM files
-        oneOf.exclude.push(wasmExtensionRegExp);
+        // Make file-loader ignore WASM files
+        oneOf.exclude.push(/\.wasm$/);
       }
     });
   });
 
-  // add a dedicated loader for WASM
-  config.module.rules.push({
-    test: wasmExtensionRegExp,
-    include: path.resolve(__dirname, "src"),
-    use: [{ loader: require.resolve("wasm-loader"), options: {} }],
-  });
+  config.plugins = (config.plugins || []).concat([
+    new WasmPackPlugin({
+      crateDirectory: path.resolve(__dirname, "./../irmaseal-js/"),
+      extraArgs: "--no-typescript",
+      outDir: path.resolve(__dirname, "./../irmaseal-js/static"),
+    }),
+  ]);
 
   return config;
 };
