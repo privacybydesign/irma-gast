b94018daf4ee2d9ab37bd7d5d1bf8fffc6bb21d6
Leon <leon.botros@ru.nl>
2020-11-05 17:07:13 +0100

    feat: add english resources

--- front-end/src/i18n.js
+++ front-end/src/i18n.js
@@ -2,22 +2,23 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 import nl from "./translations/nl.json";
+import en from "./translations/en.json";
 
 const resources = {
   nl: nl,
+  en: en,
 };
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: Object.keys(resources)[0], // Since we only support nl now, pin the language for all environments.
-
-    keySeparator: '.',
+    lng: Object.keys(resources)[0], // for now, use dutch
+    keySeparator: ".",
 
     interpolation: {
-      escapeValue: false // react already safes from xss
-    }
+      escapeValue: false, // react already safes from xss
+    },
   });
 
 export default i18n;
