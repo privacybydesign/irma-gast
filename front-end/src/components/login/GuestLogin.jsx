33f857e684a354ce4a9b382d363c0ac9dd04dc99
Leon <l.botros@cs.ru.nl>
2020-11-25 16:47:54 +0100

    make button look decent on mobile

--- front-end/src/components/login/GuestLogin.jsx
+++ front-end/src/components/login/GuestLogin.jsx
@@ -23,25 +23,18 @@ class GuestLogin extends React.Component {
         <h2>{this.props.t(`headerguest`)}</h2>
         <p className="center-content">{this.props.t(`p1guest`)}</p>
         <div>
-          <form id="contact-form" onSubmit={this.handleSubmit}>
-            <div
-              className="form-group"
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <TextField
-                type="email"
-                fullWidth
-                className="form-control"
-                aria-describedby="emailHelp"
-                value={this.state.email}
-                onChange={this.onEmailChange}
-                label={this.props.t("email")}
-                required
-              />
+          <form id="email-form" onSubmit={this.handleSubmit}>
+            <TextField
+              type="email"
+              fullWidth
+              className="form-control"
+              aria-describedby="emailHelp"
+              value={this.state.email}
+              onChange={this.onEmailChange}
+              label={this.props.t("email")}
+              required
+            />
+            <div className="flex-container flex-item-single-centered">
               <button type="submit" className="btn irma-btn-secondary">
                 {this.props.t("guestregisterbuttontext")}
               </button>{" "}
